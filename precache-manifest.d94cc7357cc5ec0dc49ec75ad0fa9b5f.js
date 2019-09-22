fd76d204f20078e1eb1197e46449226dfbbdc432
rlcdev <dev.rlc974@gmail.com>
2019-09-22 17:19:05 +0200

    Updates

---
+++ precache-manifest.d94cc7357cc5ec0dc49ec75ad0fa9b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810550a153a9a96a87c5623e921dc027",
+    "url": "/PhotoGallery/index.html"
+  },
+  {
+    "revision": "5e2c059b40262820cc3a",
+    "url": "/PhotoGallery/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "540a142ef12272896bab",
+    "url": "/PhotoGallery/static/css/main.e123068c.chunk.css"
+  },
+  {
+    "revision": "5e2c059b40262820cc3a",
+    "url": "/PhotoGallery/static/js/2.c9e43770.chunk.js"
+  },
+  {
+    "revision": "540a142ef12272896bab",
+    "url": "/PhotoGallery/static/js/main.e9df595a.chunk.js"
+  },
+  {
+    "revision": "d17c860292236c30c552",
+    "url": "/PhotoGallery/static/js/runtime-main.cf210256.js"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BasePage from './BasePage/BasePage'
-function App() {
-  return (
-    <BasePage/>
-  )
-}
-
-export default App;
